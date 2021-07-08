05e0abffb1f426ff77489e2934e3b7e3f578835e
Josh Gumperz <joshg3023@gmail.com>
2021-07-08 10:27:37 -0700

    added code to return recipe and nutritional information when user searches for a drink

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
+var searchDrink = $(".search-form")
+var drinkInput = $(".search-bar")
 
-getCockTail();
-var drink;
+searchDrink.on("submit", function(event){
+    event.preventDefault() 
+    var drink = drinkInput.val()
+    getCockTail(drink)
+    getNutritionInfo(drink)
+})
 
-function getNutritionInfo() {
+function getNutritionInfo(drink) {
     fetch("https://cors.bridged.cc/https://nutrition-api.esha.com/foods?query=" + drink + "&0&10&true", {
     headers: {
         "Ocp-Apim-Subscription-Key": "951b42ae2f4a4413a3d54640205f22c5"
@@ -18,15 +24,12 @@ function getNutritionInfo() {
 
 
 
-function getCockTail() {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+function getCockTail(drink) {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink)
     .then(function(response) {
         return response.json()
     })
     .then(function(data){
         console.log( data );
     })
-    // .then( function () {
-    //     getNutritionInfo();
-    // })
 }
