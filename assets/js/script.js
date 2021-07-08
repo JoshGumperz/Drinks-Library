39fac2303e8b4c594cb9babfb11273b6af006c56
Huiran Lin <hugh80082@gmail.com>
2021-07-07 18:38:50 -0700

    add styling to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ getCockTail();
 var drink;
 
 function getNutritionInfo() {
-    fetch("https://corsbridge.herokuapp.com/https://nutrition-api.esha.com/foods?query=" + drink + "&0&10&true", {
+    fetch("https://cors.bridged.cc/https://nutrition-api.esha.com/foods?query=" + drink + "&0&10&true", {
     headers: {
         "Ocp-Apim-Subscription-Key": "951b42ae2f4a4413a3d54640205f22c5"
     }
@@ -19,15 +19,14 @@ function getNutritionInfo() {
 
 
 function getCockTail() {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
     .then(function(response) {
         return response.json()
     })
     .then(function(data){
-        drink = data.drinks[0].strIngredient1;
-        console.log( drink );
+        console.log( data );
     })
-    .then( function () {
-        getNutritionInfo();
-    })
+    // .then( function () {
+    //     getNutritionInfo();
+    // })
 }
