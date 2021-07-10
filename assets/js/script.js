4bad7c7354a79b004f31295de3377a3deae36c05
Josh Gumperz <joshg3023@gmail.com>
2021-07-09 21:07:12 -0700

    merged code and designed content card to display when user searches a cocktail

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ var occasions = [
         categories : [ "Shot", "Beer" ]
     },
     Promotion = {
-        description : "Promotin",
+        description : "Promotion",
         categories : [ "Shot", "Beer" ]
     },
     Birthday = { 
@@ -53,96 +53,67 @@ var occasions = [
 
 
 // getCategories();
-getCockTail();
-getDrinksByOccasions();
-
-var drink;
-
-function getDrinksByOccasions() {
-    // for( var occasion of occasions ) {
-    //     for( var category of occasion.categories ) {
-    //         getDrinksByOccasionsHelper( category );
-    //     }
-    // }
-    var drinkNameEl = $('<h2>' );
-    drinkNameEl.text( "Occasion: " + occasions[0].description );
-    $( '.drinkName-container' ).append( drinkNameEl );
-    for( var category of occasions[0].categories ) {
-        getDrinksByOccasionsHelper( category );
-    }
-}
-
-function getDrinksByOccasionsHelper( category ) {
-    fetch( "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=" + category )
-    .then( function ( response ) {
-        return response.json();
-    })
-    .then( function( data ) {
-        return data;
-    })
-    .then( function ( categories ) {
-        for( var each of categories.drinks ) {
-            displayRecipe( each );
-        }
-    })
-}
-
-
-function displayRecipe( drink ) {
-    var drinkNameEl = $( '<h3>' );
-    drinkNameEl.text( drink.strDrink );
-    $('.drinkName-container').append( drinkNameEl );
-
-    var drinkID = drink.idDrink;
-    getCockTailRecipeByID( drinkID );
-}
-
-
-function getNutritionInfo() {
-    fetch("https://cors.bridged.cc/https://nutrition-api.esha.com/foods?query=" + drink + "&0&10&true", {
-    headers: {
-        "Ocp-Apim-Subscription-Key": "951b42ae2f4a4413a3d54640205f22c5"
-    }
-    })
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-    })
-}
+// getCockTail();
+// getDrinksByOccasions();
 
 
 
-function getCockTail(drink) {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-        var drinkID = data.drinks[0].idDrink
-        getCockTailRecipeByID(drinkID)
-    })
-}
+// function getDrinksByOccasions() {
+//     // for( var occasion of occasions ) {
+//     //     for( var category of occasion.categories ) {
+//     //         getDrinksByOccasionsHelper( category );
+//     //     }
+//     // }
+//     var drinkNameEl = $('<h2>' );
+//     drinkNameEl.text( "Occasion: " + occasions[0].description );
+//     $( '.drinkName-container' ).append( drinkNameEl );
+//     for( var category of occasions[0].categories ) {
+//         getDrinksByOccasionsHelper( category );
+//     }
+// }
 
-function getCockTailRecipeByID( id ) {
-    fetch("https://cors.bridged.cc/http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007")
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data){
-        console.log( data );
-        var recipeEl = $( '<p>' );
-        recipeEl.text( data.drinks[0].strInstructions );
-        $( '.recipe-container' ).append( recipeEl );
-    })
-    // .then( function () {
-    //     getNutritionInfo();
-    // })
-}
+// function getDrinksByOccasionsHelper( category ) {
+//     fetch( "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=" + category )
+//     .then( function ( response ) {
+//         return response.json();
+//     })
+//     .then( function( data ) {
+//         // console.log( data );
+//         for( var each of data.drinks ) {
+//             displayRecipe( each );
+//         }
+//         return data;
+//     })
+// }
 
-// test function
+
+// function displayRecipe( drink ) {
+//     var drinkNameEl = $( '<h3>' );
+//     drinkNameEl.text( drink.strDrink );
+//     $('.drinkName-container').append( drinkNameEl );
+
+//     var drinkID = drink.idDrink;
+//     return getCockTailRecipeByID( drinkID );
+// }
+
+
+// function getNutritionInfo() {
+//     fetch("https://cors.bridged.cc/https://nutrition-api.esha.com/foods?query=" + drink + "&0&10&true", {
+//     headers: {
+//         "Ocp-Apim-Subscription-Key": "951b42ae2f4a4413a3d54640205f22c5"
+//     }
+//     })
+//     .then(function(response) {
+//         return response.json()
+//     })
+//     .then(function(data){
+//         console.log(data)
+//     })
+// }
+
+
+
+// For testing purposes
 // function getCategories() {
 //     fetch( "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list" )
 //     .then( function( response ) {
@@ -152,3 +123,84 @@ function getCockTailRecipeByID( id ) {
 //         console.log( data );
 //     })
 // }
+
+
+var drink;
+var drinksDisplayedSoFar = 0;
+var drinksArr = [];
+localStorage.setItem( "drinksArr", JSON.stringify( drinksArr ) );
+
+var inputOccasions = [ "Formal Party" ];
+var recipesData = [];
+var recipesDisplayed = 0;
+
+handleUserInputOccasions();
+// console.log( recipesData );
+
+
+async function handleUserInputOccasions() {
+
+    for( var each of inputOccasions ) {
+        for( var j of occasions ) {
+            if( each === j.description ) {
+                getDrinksByCategories( j.categories );
+            }
+        }
+    }
+    
+}
+
+
+function getDrinksByCategories( categories ) {
+    for( var category of categories ) {
+        fetch( "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=" + category )
+        .then( function ( response ) {
+            return response.json();
+        })
+        .then( function( data ) {
+            console.log( data );
+            for( var each of data.drinks) {
+                getCockTailRecipeByID( each.idDrink );
+            }
+        })
+    }
+}
+
+
+function getCockTailRecipeByID( id ) {
+    fetch("https://cors.bridged.cc/http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + id )
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data){
+        // console.log( data );
+        storeData( data );
+    })
+    .then( function () {
+        displayDrinkData();
+    })
+}
+
+
+function storeData( data ) {
+    var drinksArr = JSON.parse( localStorage.getItem( "drinksArr" ) );
+    drinksArr.push( data.drinks[0] );
+    // console.log( drinksArr );
+    localStorage.setItem( "drinksArr", JSON.stringify( drinksArr ) );
+}
+
+
+function displayDrinkData( ) {
+    var drinksArr = JSON.parse( localStorage.getItem( "drinksArr" ) );
+    // console.log( drinksArr.length );
+
+    var drinkName = drinksArr[ drinksArr.length - 1 ].strDrink;
+    
+    // var btnEl = $( '#' + ( drinksDisplayedSoFar + 1 ) ).children('button');
+    // console.log( btnEl.textContent );
+
+    var btnEl = $( '#1' ).children('button');
+    console.log( btnEl.text() );
+    drinksDisplayedSoFar++;
+
+}
